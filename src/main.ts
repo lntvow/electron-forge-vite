83d3a9ae8e3036e522d1edbd0fd17ac22c8f5672
lntvow <lntvow@qq.com>
2025-05-15 16:30:08 +0800

    111

---
+++ src/main.ts
@@ -0,0 +1,104 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'node:path'
+import started from 'electron-squirrel-startup'
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (started) {
+  app.quit()
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
+
+  // and load the index.html of the app.
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
+  } else {
+    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`))
+  }
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.on('devtools-opened', () => {
+    const css = `
+        :root {
+            --sys-color-base: var(--ref-palette-neutral100);
+            --source-code-font-family: consolas !important;
+            --source-code-font-size: 12px;
+            --monospace-font-family: consolas !important;
+            --monospace-font-size: 12px;
+            --default-font-family: system-ui, sans-serif;
+            --default-font-size: 12px;
+            --ref-palette-neutral99: #ffffffff;
+        }
+        .theme-with-dark-background {
+            --sys-color-base: var(--ref-palette-secondary25);
+        }
+        body {
+            --default-font-family: system-ui,sans-serif;
+        }
+    `
+    mainWindow.webContents.devToolsWebContents.executeJavaScript(`
+        const overriddenStyle = document.createElement('style');
+        overriddenStyle.innerHTML = '${css.replaceAll('\n', ' ')}';
+        document.body.append(overriddenStyle);
+        document.querySelectorAll('.platform-windows').forEach(el => el.classList.remove('platform-windows'));
+        addStyleToAutoComplete();
+        const observer = new MutationObserver((mutationList, observer) => {
+            for (const mutation of mutationList) {
+                if (mutation.type === 'childList') {
+                    for (let i = 0; i < mutation.addedNodes.length; i++) {
+                        const item = mutation.addedNodes[i];
+                        if (item.classList.contains('editor-tooltip-host')) {
+                            addStyleToAutoComplete();
+                        }
+                    }
+                }
+            }
+        });
+        observer.observe(document.body, {childList: true});
+        function addStyleToAutoComplete() {
+            document.querySelectorAll('.editor-tooltip-host').forEach(element => {
+                if (element.shadowRoot.querySelectorAll('[data-key="overridden-dev-tools-font"]').length === 0) {
+                    const overriddenStyle = document.createElement('style');
+                    overriddenStyle.setAttribute('data-key', 'overridden-dev-tools-font');
+                    overriddenStyle.innerHTML = '.cm-tooltip-autocomplete ul[role=listbox] {font-family: consolas !important;}';
+                    element.shadowRoot.append(overriddenStyle);
+                }
+            });
+        }
+    `)
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
